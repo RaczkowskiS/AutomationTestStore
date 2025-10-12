466eb23ca14d4be6d39157a44427c9cf923ebdf8
Sebastian <sebastian.r1001@gmail.com>
2025-10-12 10:35:12 +0200

    ignore setup for one test

--- src/tests/ui/hc/healthcheck.spec.ts
+++ src/tests/ui/hc/healthcheck.spec.ts
@@ -1,20 +1,32 @@
 import { test, expect } from '../../../fixtures/base';
 import { users } from '../../../testdata/users'
 
-test('should open automationteststore page successfully', async ({ homePage }) => {
-  await test.step('Open home page', async () => {
-      await homePage.open();
-      await homePage.expectLoaded();
+test.describe(() => {
+  test.use({ storageState: { cookies: [], origins: [] } });
+
+  test('should open automationteststore page successfully', async ({ homePage, page }) => {
+    await test.step('Open home page', async () => {
+        await homePage.open();
+        await homePage.expectLoaded();
+    });
+
+    await test.step('Check if user is log out', async () => {
+        expect(await homePage.isUserAlreadyLoggedIn(users.user2.name)).toBeFalsy();
+    });
   });
 });
 
-test('should open home for user already logged in', async ({ homePage }) => {
-    await test.step('Open home page', async () => {
-      await homePage.open();
-      await homePage.expectLoaded();
-    });
+test.describe(() => {
+  test.use({ storageState: '.auth/user.json' });
 
-    await test.step('Check if user is logged in', async () => {
-        expect(await homePage.isUserAlreadyLoggedIn(users.user2.name)).toBeTruthy();
-    });
+  test('should open home for user already logged in', async ({ homePage }) => {
+      await test.step('Open home page', async () => {
+        await homePage.open();
+        await homePage.expectLoaded();
+      });
+
+      await test.step('Check if user is logged in', async () => {
+          expect(await homePage.isUserAlreadyLoggedIn(users.user2.name)).toBeTruthy();
+      });
+  });
 });
